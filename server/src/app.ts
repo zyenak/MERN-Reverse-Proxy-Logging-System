3bdd0d0676a3509484c48d549177d1f79b047785
zyena-kamran <158252686+zyena-kamran@users.noreply.github.com>
2025-07-04 23:18:36 +0500

    project setup

---
+++ server/src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (_, res) => {
+  res.send('Reverse Proxy Logger API');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
